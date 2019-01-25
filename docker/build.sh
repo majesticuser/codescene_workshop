17a73a9681cea299b3616e464b5c91842a0e773f
Virginio Carfagno <virginio.carfagno@mindmatters.de>
2019-01-25 13:59:06 +0100

    Setup for CodeScene workshop

---
+++ docker/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+cd "$(dirname "$0")"
+
+docker build -t codescene .
---
+++ docker/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+cd "$(dirname "$0")/.."
+
+docker run --rm -p "3003:3003" \
+  -v "$(pwd)"/repos:/repos \
+  -v "$(pwd)"/resources:/resources \
+  codescene
